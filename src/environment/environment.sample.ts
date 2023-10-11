67c08c2f9a1c4f4a7b8e4b5ab402d10c9b1ee52f
apaolaoliveira <paolaoliveira.dev@gmail.com>
2023-10-11 16:21:22 -0300

    add environments

--- src/environment/environment.sample.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-    API_KEY: '' // here you need to add your API Read Access Token from TMDb
-}
---
+++ src/environment/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    API_KEY: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMjgwNGUxYmRhZmZlNTliNWFkZGMxMzJlZjUxMDBmZiIsInN1YiI6IjY1MDllNThhYWVkZTU5MDBjNGI0ZGUzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wV9tJmmHcOdywbukVX0V9wdnmU2VFx_eqc981eQwY8s' 
+}
